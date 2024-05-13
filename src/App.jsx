2684b4beeb044f53d391288f982f5e5a96cbb8cc
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-05-13 20:00:34 +0600

    handle event

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 
 function App() {
+  function handleClick() {
+    alert('Button clicked');
+  }
+
+  const handleClick2 = () => {
+    alert('Button 2 clicked');
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h1>React</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => {
+        alert('Third clicked');
+      }}>Third</button>
+      <button onClick={() => addToFive(2)}>Four</button>
     </>
+
+
   )
 }
 
