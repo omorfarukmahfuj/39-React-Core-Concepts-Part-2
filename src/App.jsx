5e2dee69e2d40d387d09972c3658d9c901899bfe
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-05-14 12:16:04 +0600

    explore event, state, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+import Counter from './Counter';
+import Team from './Team';
+import Users from './Users';
+import Friends from './Friends';
 
 function App() {
   function handleClick() {
@@ -15,7 +19,11 @@ function App() {
 
   return (
     <>
-      <h1>React</h1>
+      <h3>React</h3>
+      <Friends></Friends>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
       <button onClick={handleClick2}>Click 2</button>
       <button onClick={() => {
